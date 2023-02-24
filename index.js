5f0e7ba0a75ab4eae26d3439a25582c0186ed01b
aria231 <aria231@students.cs.ubc.ca>
2023-02-23 23:43:07 -0800

    format skills icons

--- index.js
+++ index.js
@@ -22,8 +22,33 @@ import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js"
 //     }
 // });
 
-/* SCROLL TRACKING
- * Cite: https://youtu.be/VgS5CP7zlXE */
+/**
+ * CHECK IF ELEMENT IS IN VIEWPORT
+ * Cite: https://www.javascripttutorial.net/dom/css/check-if-an-element-is-visible-in-the-viewport/
+ */
+function isInViewport(elem) {
+    let boundingRect = elem.getBoundingClientRect();
+    return (boundingRect.top >= 0 
+        && boundingRect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+        && boundingRect.left >= 0
+        && boundingRect.right <= (window.innerWidth || document.documentElement.clientWidth));
+}
+
+const divElems = document.querySelectorAll(".section");
+
+document.addEventListener('scroll', function() {
+    divElems.forEach(elem => {
+        if(isInViewport(elem)) {
+            elem.classList.add("fade-in-element");
+            elem.classList.remove("hidden");
+        }
+    })
+})
+
+/**
+ * SCROLL TRACKING
+ * Cite: https://youtu.be/VgS5CP7zlXE 
+ */
 const scrollTracker = document.querySelector(".scroll-tracker");
 
 const scrollTrackingTimeline = new ScrollTimeline({
@@ -41,3 +66,88 @@ scrollTracker.animate(
         timeline: scrollTrackingTimeline,
     }
 );
+
+/**
+ * SECTION ANIMATION
+ */
+const animatedSections = document.querySelectorAll(".section");
+
+// animatedSections.forEach(section => {
+//     const sectionOffsetTop = section.offsetTop;
+//     const sectionHeight = section.offsetHeight;
+
+//     section.animate(
+//         {
+//             opacity: ["0", "1"]
+//         },
+//         {
+//             duration: 1,
+//             timeline: new ScrollTimeline({
+//                 scrollOffsets: [
+//                     // CSS.px(sectionOffsetTop + sectionHeight - window.innerHeight),
+//                     // CSS.px(sectionOffsetTop - 200),
+//                     {target: section, edge: "end", threshold: "1"},
+//                     {target: section, edge: "start", threshold: "1"},
+//                 ]
+//             })
+//         }
+//     )
+// });
+
+/**
+ * Image Animation
+ * Cite: https://youtu.be/VgS5CP7zlXE
+ */
+const animatedImages = document.querySelectorAll(".image-rotate-in");
+
+animatedImages.forEach(image => {
+    const imageOffsetTop = image.offsetTop;
+    const imageHeight = image.offsetHeight;
+
+    const animatedImageTimeline = new ScrollTimeline({
+        // scrollOffsets: [
+        //     {target: image, edge: "end", threshold: "0"},
+        //     {target: image, edge: "start", threshold: "1"}
+        // ],
+        scrollOffsets: [
+            CSS.px(imageOffsetTop + imageHeight - window.innerHeight),
+            CSS.px(imageOffsetTop - 300),
+        ],
+    });
+
+    image.animate(
+        {
+            transform: [
+                "perspective(1000px) rotateX(45deg)", 
+                "perspective(1000px) rotate(0)",
+            ],
+            opacity: ["0", "1"]
+        },
+        {
+            duration: 1,
+            easing: "linear",
+            timeline: animatedImageTimeline,
+        }
+    )
+});
+
+// const animatedImageTimeline = new ScrollTimeline({
+//     scrollOffsets: [
+//         {target: animatedImage, edge: "end", threshold: "0"},
+//         {target: animatedImage, edge: "start", threshold: "1"}
+//     ],
+// });
+
+// animatedImage.animate(
+//     {
+//         transform: [
+//             "perspective(1000px) rotateX(45deg)", 
+//             "perspective(1000px) rotate(0)",
+//         ],
+//         opacity: ["0", "1"]
+//     },
+//     {
+//         duration: 1, 
+//         timeline: animatedImageTimeline,
+//     }
+// )
