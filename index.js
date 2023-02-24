02561eabdfaebf4aea248a3122967e5d6027770e
aria231 <aria231@students.cs.ubc.ca>
2023-02-24 02:30:14 -0800

    placeholders for experience section

--- index.js
+++ index.js
@@ -151,3 +151,24 @@ animatedImages.forEach(image => {
 //         timeline: animatedImageTimeline,
 //     }
 // )
+
+
+/**
+ * EXPERIENCE SECTION
+ */
+
+/* Cite: https://www.w3schools.com/howto/howto_js_accordion.asp */
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i=0; i<acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.display == "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
